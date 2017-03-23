f772c8b957297f7c06fd2abef09ed746e1f872bc
Gloria Friesen and Riley Watts <gloria@>
2017-03-23 16:47:36 -0700

    add chili/chiliId/bean/beanId route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -57,12 +57,22 @@ public class App {
 
     post("/chilis/:chili_id/bean", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      Chili newChili = Chili.find(Integer.parseInt(request.params(":chili_id")));
-      model.put("chili", newChili);
-      model.put("template", "templates/new-bean.vtl");
       String bean = request.queryParams("bean");
       Bean newBean = new Bean(bean);
+      request.session().attribute("beanPrime", newBean.all());
+      Chili newChili = Chili.find(Integer.parseInt(request.params(":chili_id")));
+      model.put("chili", newChili);
+      model.put("beanKey", newBean.all());
+      model.put("template", "templates/new-bean.vtl");
       model.put("bean", newBean);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/chilis/:chili_id/bean/:bean_id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Bean newBean = Bean.find(Integer.parseInt(request.params(":bean_id")));
+      model.put("bean", newBean);
+      model.put("template", "templates/bean.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
